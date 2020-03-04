6e4a79c2eddffd383dea929adda0ff46ed7d1c3b
jdbakes10 <60588671+jdbakes10@users.noreply.github.com>
2020-03-04 15:26:01 -0600

    Changed vars
    She told me to remove vars

--- forms/animalExtraXP/animalExtraXP.js
+++ forms/animalExtraXP/animalExtraXP.js
@@ -1,8 +1,8 @@
 /*
 for (let i = 0; i<2; i++) {
-  var myAnimals = ["dog", "cat", "horse", "mouse", "pig", "cow", "ferret", "lizard", "frog"]
-  var enter = prompt("Enter the name of an animal to add it to the array")
-  var newAnimal = enter.toLowerCase()
+  myAnimals = ["dog", "cat", "horse", "mouse", "pig", "cow", "ferret", "lizard", "frog"]
+  enter = prompt("Enter the name of an animal to add it to the array")
+  newAnimal = enter.toLowerCase()
   myAnimals.push(newAnimal)
   alert(`The last animal is a/an ${myAnimals[myAnimals.length - 1]}`)
 }
